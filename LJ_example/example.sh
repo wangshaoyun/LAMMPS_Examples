3882c7075dfc44c8758fa946e1f77806c25f09c9
wangshaoyun <wangshaoyun88@163.com>
2021-06-01 16:30:01 -0500

    Update example.sh

--- LJ_example/example.sh
+++ LJ_example/example.sh
@@ -3,5 +3,5 @@
 set echo
 
 ln -s ../LJ_initial_cfgs/in.nvt.dens_0.4000 ./
-mpirun -np 8 lmp_mpi -in NVT.startfromrestart -var rho 0.4000 -var temp 1.50
+mpirun -np 2 lmp_mpi -in NVT.startfromrestart -var rho 0.4000 -var temp 1.50
 ../analysis/block_analysis.py -f ave.dens_0.4000.out -b 5 -m 100000
